e51c75912ec00bd514aa6a81221704b011ecfc67
1pha <1phantasmas@korea.ac.kr>
2023-03-05 11:12:17 +0000

    Fix processing data

--- erc/datasets.py
+++ erc/datasets.py
@@ -1,9 +1,17 @@
+import os
 from glob import glob
+import logging
 from pathlib import Path
 
 import pandas as pd
+import omegaconf
+from tqdm import tqdm
 import torch
 from torch.utils.data import Dataset
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class KEMDy19Dataset(Dataset):
@@ -11,17 +19,18 @@ class KEMDy19Dataset(Dataset):
     eda_path_fmt = "./data/KEMDy19/EDA/Session{0}/Original/Sess{0}{1}.csv"
 
     # Annotation (ECG / EDA / Emotion / Valence & Arousal)
-    MALE_ANN_PATH = "{}/annotation/Session*_M_*"
-    FEMALE_ANN_PATH = "{}/annotation/Session*_F_*"
-    TOTAL_DF_PATH = "./kemdy19.csv"
+    male_annot_expr = "./annotation/Session*_M_*"
+    female_annot_expr = "./annotation/Session*_F_*"
+    TOTAL_DF_PATH = "./data/kemdy19.csv"
 
-    def __init__(self, cfg: dict):
+    def __init__(self, base_path):
         """
         Args:
             cfg: yaml file
         """
-        self._base_path: str = cfg["BASE_PATH"]
-        self.total_df: pd.DataFrame = self.processed_db(self)
+        logger.info("Instantiate KEMDy19 Dataset")
+        self.base_path: Path = Path(base_path)
+        self.total_df: pd.DataFrame = self.processed_db()
 
     def __len__(self):
         return len(self.total_df)
@@ -42,13 +51,22 @@ class KEMDy19Dataset(Dataset):
         sample = {"wav": None, "txt": None, "eda": None, "temp": None, "label": label}
         return sample
 
-    @property
-    def processed_db(self):
-        if not isinstance(self.TOTAL_DF_PATH, Path):
-            self.make_total_df()
-        return pd.read_csv(self.TOTAL_DF_PATH)
+    def processed_db(self) -> pd.DataFrame:
+        """ Reads in .csv file if exists.
+        If pre-processed .csv file does NOT exists, read from data path. """
+        if not os.path.exists(self.TOTAL_DF_PATH):
+            logger.info(f"{self.TOTAL_DF_PATH} does not exists. Process from raw data")
+            total_df = self.make_total_df()
+        else:
+            try:
+                total_df = pd.read_csv(self.TOTAL_DF_PATH)
+            except pd.errors.EmptyDataError as e:
+                logger.error(f"{self.TOTAL_DF_PATH} seems to be empty")
+                logger.exception(e)
+                total_df = None
+        return total_df
 
-    def make_total_df(self) -> None:
+    def make_total_df(self) -> pd.DataFrame:
         selected_columns = [
             "Numb",
             "Wav",
@@ -62,11 +80,18 @@ class KEMDy19Dataset(Dataset):
             "Segment ID",
             "Total Evaluation",
         ]
-        male_annotations = sorted(glob(self.MALE_ANN_PATH.format(self._base_path)))
-        female_annotations = sorted(glob(self.FEMALE_ANN_PATH.format(self._base_path)))
+        male_annots = sorted(self.base_path.rglob(self.male_annot_expr))
+        female_annots = sorted(self.base_path.rglob(self.female_annot_expr))
+        assert (len(male_annots) > 0) & (len(female_annots) > 0),\
+            f"Annotations does not exists. Check {self.base_path / self.male_annot_expr}"
 
         total_df = pd.DataFrame()
-        for m_ann, f_ann in zip(male_annotations, female_annotations):
+        pbar = tqdm(
+            iterable=zip(male_annots, female_annots),
+            desc="Processing ECG/EDA/Label",
+            total=len(male_annots)
+        )
+        for m_ann, f_ann in pbar:
             m_df = pd.read_csv(m_ann).dropna()[selected_columns]
             f_df = pd.read_csv(f_ann).dropna()[selected_columns]
 
@@ -87,7 +112,10 @@ class KEMDy19Dataset(Dataset):
             # 다시 합쳐서 정렬
             tmp_df = pd.concat([m_df, f_df], axis=0).sort_values("Numb")
             total_df = pd.concat([total_df, tmp_df], axis=0)
+        
+        logger.info(f"New dataframe saved as {self.TOTAL_DF_PATH}")
         total_df.to_csv(self.TOTAL_DF_PATH, index=False)
+        return total_df
 
     @staticmethod
     def str2num(key) -> int:
