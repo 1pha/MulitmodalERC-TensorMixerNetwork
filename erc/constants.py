b4047ce2c443d6879189bdee17d8083a5c6e42a3
hoesungryu <hoesungryu@korea.ac.kr>
2023-03-09 10:58:35 +0000

    wav2vec config

--- erc/constants.py
+++ erc/constants.py
@@ -20,6 +20,17 @@ emotion2idx = {
     "disgust": 6,
 }
 
+# idx mapper to emotion
+idx2emotion = {
+    0: 'surprise',
+    1: 'fear',
+    2: 'angry',
+    3: 'neutral',
+    4: 'happy',
+    5: 'sad',
+    6: 'disgust'
+}
+
 # Gender Mapper
 gender2idx = {
     "M": 0,
