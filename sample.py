541a1c33846f00b80961c1e3d865533713f6152c
1pha <1phantasmas@korea.ac.kr>
2023-03-09 09:09:04 +0000

    Fix accelerate

--- sample.py
+++ sample.py
@@ -2,11 +2,11 @@ import hydra
 import omegaconf
 import torch.nn as nn 
 from torch.utils.data import DataLoader
-from accelerate import Accelerator
+from accelerate import Accelerator, DistributedDataParallelKwargs
 from transformers import Wav2Vec2Processor, Wav2Vec2ForSequenceClassification
 
-# from erc.utils import get_logger
-# logger = get_logger()
+from erc.utils import get_logger
+logger = get_logger()
 
 
 def loop(batch, model, criterion, optimizer, accelerator):
@@ -15,13 +15,11 @@ def loop(batch, model, criterion, optimizer, accelerator):
     inputs = {"input_values": batch['wav'],
             "attention_mask": batch['wav_mask']}
     logits = model(**inputs).logits
-
     loss = criterion(logits, labels.long())
 
     optimizer.zero_grad()
     accelerator.backward(loss)
     optimizer.step()
-
 
 
 @hydra.main(config_path="./config", config_name="config", version_base="1.3")
@@ -38,8 +36,8 @@ def main(cfg: omegaconf.DictConfig):
     optimizer = hydra.utils.instantiate(cfg.optimizer, params=model.parameters())
 
     train_loader = hydra.utils.instantiate(cfg.dataloader, dataset=train_dataset)
-    accelerator = Accelerator(device_placement=True,
-                              split_batches=False)
+    ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+    accelerator = Accelerator(device_placement=True, kwargs_handlers=[ddp_kwargs])
     model, optimizer, train_loader, criterion = accelerator.prepare(
         model, optimizer, train_loader, criterion)
     
@@ -47,8 +45,6 @@ def main(cfg: omegaconf.DictConfig):
     for batch_idx, batch in enumerate(train_loader): 
         loop(batch=batch, model=model, criterion=criterion, optimizer=optimizer, accelerator=accelerator)
         accelerator.wait_for_everyone()
-
-
     return
 
 if __name__=="__main__":
