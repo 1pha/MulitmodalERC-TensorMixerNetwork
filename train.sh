ff980b21d05b53d8c981ad7a0f185e0f61064404
1pha <1phantasmas@korea.ac.kr>
2023-03-16 09:24:42 +0000

    Fix checkpointing trigger

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ read -p "Enter devices. For multiple devices, separate with comma [0]: " device
 device=${device:-0}
 
 echo "Create/Check valfold=${valfold} dataset"
-python -m fire erc.datasets HF_KEMD --mode=train --validation_fold=${valfold} --multilabel=True --num_proc=1
-python -m fire erc.datasets HF_KEMD --mode=valid --validation_fold=${valfold} --multilabel=True --num_proc=1
+python -m fire erc.datasets HF_KEMD --mode=train --validation_fold=${valfold}
+python -m fire erc.datasets HF_KEMD --mode=valid --validation_fold=${valfold}
 echo "Start training with device=${device}"
-# CUDA_VISIBLE_DEVICES=${device} python train.py
+CUDA_VISIBLE_DEVICES=${device} python train.py
