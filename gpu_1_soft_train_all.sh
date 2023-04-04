aa66d01b97e359c4efa77a006a4fa8bffbacd7d1
hoesungryu <hoesungryu@korea.ac.kr>
2023-04-04 10:17:25 +0000

    change .sh

--- gpu_1_soft_train_all.sh
+++ gpu_1_soft_train_all.sh
@@ -30,9 +30,17 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=1
 
+# for fold in $(seq 0 4) # k-fold
+# do
+#     python -m fire erc.datasets HF_KEMD --mode=train --multilabel=False --validation_fold=$fold --remove_deuce=True
+#     python -m fire erc.datasets HF_KEMD --mode=valid --multilabel=False --validation_fold=$fold --remove_deuce=True
+#     python train.py model=mlp_mixer_roberta model.config.txt=klue/roberta-large dataset.validation_fold=$fold dataset.multilabel=False dataset.remove_deuce=True dataloader.batch_size=6 module.load_from_checkpoint=weights_AI_HUB/RobertaL_pretrained_aihub_weight.ckpt
+# done
+
+# True 안 했음... 
 for fold in $(seq 0 4) # k-fold
 do
-    python -m fire erc.datasets HF_KEMD --mode=train --multilabel=False --validation_fold=$fold --remove_deuce=True
-    python -m fire erc.datasets HF_KEMD --mode=valid --multilabel=False --validation_fold=$fold --remove_deuce=True
-    python train.py model=mlp_mixer_roberta model.config.txt=klue/roberta-large dataset.validation_fold=$fold dataset.multilabel=False dataset.remove_deuce=True dataloader.batch_size=6 module.load_from_checkpoint=weights_AI_HUB/RobertaL_pretrained_aihub_weight.ckpt
+    python -m fire erc.datasets HF_KEMD --mode=train --multilabel=True --validation_fold=$fold --remove_deuce=False
+    python -m fire erc.datasets HF_KEMD --mode=valid --multilabel=True --validation_fold=$fold --remove_deuce=False
+    python train.py model=mlp_mixer_roberta model.config.txt=klue/roberta-large dataset.validation_fold=$fold dataset.multilabel=True dataset.remove_deuce=False +model.use_peakl=True dataloader.batch_size=6 module.load_from_checkpoint=weights_AI_HUB/RobertaL_pretrained_aihub_weight.ckpt
 done
--- rest_train.sh
+++ rest_train.sh
@@ -9,9 +9,9 @@ python train.py model=mlp_mixer_roberta model.config.txt=klue/roberta-large data
 python train.py model=mlp_mixer_roberta model.config.txt=klue/roberta-large dataset.validation_fold=4 dataset.multilabel=True dataset.remove_deuce=False +model.use_peakl=False
 
 # True 안 했음... 
-for fold in $(seq 0 4) # k-fold
-do
-    python -m fire erc.datasets HF_KEMD --mode=train --multilabel=True --validation_fold=$fold --remove_deuce=False
-    python -m fire erc.datasets HF_KEMD --mode=valid --multilabel=True --validation_fold=$fold --remove_deuce=False
-    python train.py model=mlp_mixer_roberta model.config.txt=klue/roberta-large dataset.validation_fold=$fold dataset.multilabel=True dataset.remove_deuce=False +model.use_peakl=True dataloader.batch_size=6 module.load_from_checkpoint=weights_AI_HUB/RobertaL_pretrained_aihub_weight.ckpt
-done
+# for fold in $(seq 0 4) # k-fold
+# do
+#     python -m fire erc.datasets HF_KEMD --mode=train --multilabel=True --validation_fold=$fold --remove_deuce=False
+#     python -m fire erc.datasets HF_KEMD --mode=valid --multilabel=True --validation_fold=$fold --remove_deuce=False
+#     python train.py model=mlp_mixer_roberta model.config.txt=klue/roberta-large dataset.validation_fold=$fold dataset.multilabel=True dataset.remove_deuce=False +model.use_peakl=True dataloader.batch_size=6 module.load_from_checkpoint=weights_AI_HUB/RobertaL_pretrained_aihub_weight.ckpt
+# done
