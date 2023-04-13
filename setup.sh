1ca1ce6d4153eced624dd3f3d17357b2e014fcc5
1pha <1phantasmas@korea.ac.kr>
2023-04-13 12:44:54 +0000

    Final brushup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
 pip install torch==1.13.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
 pip3 install -r requirements.txt
 pip3 install -q git+https://github.com/huggingface/peft.git
-pip3 install --upgrade hydra-core
